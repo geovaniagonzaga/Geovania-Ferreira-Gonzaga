7e44d456710c1ac3d7ce672c2616bd6f4826d652
Geovania gonzaga <geovaniaferreiragonzaga634@gmail.com>
2025-08-13 21:52:00 -0300

    codigos da aula01

---
+++ aulas/aula01/meu_programa.c
@@ -0,0 +1,9 @@
+#include <stdio.h>   //inclusao de bibliotecas 
+#include <math.h>
+
+// funçao principal 
+
+int main () {
+    printf("Boa noite!\n");
+    return 0; // indica terminou o programa 
+}
